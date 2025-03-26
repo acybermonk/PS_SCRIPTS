789b6850a157937b76808a8188564d18cc3325e0
DK <86087138+acybermonk@users.noreply.github.com>
2025-03-26 09:36:45 -0500

    Update CreateExecutible.ps1

--- CreateExecutible.ps1
+++ CreateExecutible.ps1
@@ -1,3 +1,4 @@
 #Import-Module ps2exe
 #Install-Module -Name ps2exe -Force -Confirm:$false -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
-Invoke-ps2exe -inputFile "C:\Users\Cybermonk\Proton Drive\for.captcha.only\My files\Code\Testing\RP.ps1" -outputFile RP.exe -noConsole -title "RiP" -version "1.0" -x64 -copyright "October 2024" -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+cd "C:\Users\Cybermonk\Proton Drive\for.captcha.only\My files\Code\Testing"
+Invoke-ps2exe -inputFile "C:\Users\Cybermonk\Proton Drive\for.captcha.only\My files\Code\Testing\RP.ps1" -outputFile RP.exe -iconFile "C:\Users\Cybermonk\Proton Drive\for.captcha.only\My files\Code\Testing\iconRP_64.ico" -noConsole -title "RiP" -version "1.0" -x64 -copyright "October 2024" -requireAdmin -Verbose
