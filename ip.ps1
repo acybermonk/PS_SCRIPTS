54d177256b432cae0d55964ef631e950440fce23
DK <86087138+acybermonk@users.noreply.github.com>
2025-03-29 13:06:03 -0500

    Add files via upload

---
+++ ip.ps1
@@ -0,0 +1,14 @@
+﻿$ipconf = ipconfig
+[string]$ipv4 = $null
+foreach ($line in $ipconf){
+    if ($line -like "*IPv4 Address*"){
+        # Found
+        $ipv4 = $line
+        break
+    }
+}
+
+$ipline = $($ipv4.Trim()) -split ":"
+$ipv4 = $($ipline[1]).Trim()
+Write-Host $ipv4
+#195.72.49.1
